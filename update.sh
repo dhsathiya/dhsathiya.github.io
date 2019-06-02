7ae94679900ca5ed1c026115ab66698ce613e97a
dhsathiya <devarshisathiya5@gmail.com>
2019-06-02 20:09:23 +0530

    auto backup Sun Jun  2 20:09:23 IST 2019

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+rsync -avzh ~/HDD/dhsathiya.github.io/_site/* .
+rsync -avzh ~/HDD/dhsathiya.github.io/_posts/* ./_posts 
+rsync -avzh ~/HDD/dhsathiya.github.io/_config.yml .
+rsync -avzh ~/HDD/dhsathiya.github.io/ backup
 git add .
 git commit -m "auto backup $(date)"
 git push origin master
